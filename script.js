dbe711488a00dc6db5f22f8f3504b492c9bf054a
Hor5eman <120131245+Hor5eman@users.noreply.github.com>
2024-08-18 22:18:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function isMobileDevice() {
 }
 function blockMobileDevices() {
     if (isMobileDevice()) {
-        document.body.innerHTML = "<h1>Access Denied</h1><p>It looks like you are using a mobile device. Please switch to a desktop device. I'll try to fix it as soon as possible! :D - (Skill issue I dont know how to code)</p>";
+         window.location.href = "https://hor5eman.github.io/posts";
     }
 }
 window.onload = blockMobileDevices;
