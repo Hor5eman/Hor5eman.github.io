cfa0227d27c28ce521fc3bd3800f7f9ffdf145ba
Hor5eman <120131245+Hor5eman@users.noreply.github.com>
2024-08-18 22:37:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var md = new MobileDetect(window.navigator.userAgent);
 
 if (md.mobile()) {
     console.log("Mobile device detected. Redirecting...");
-    window.location.href = "https://hor5eman.github.io/posts";
+    window.location.href = "https://hor5eman.github.io/access_forbidden";
 } else {
     console.log("Desktop or non-mobile device detected.");
 }
